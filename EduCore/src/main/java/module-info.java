68927158e3baf0daaf11965ea041134a9d56d407
hem <hem@quicktext.im>
2026-06-05 16:45:53 +0100

    refactor: add module configuration - Add module-info.java for module declarations - Complete DTO structure with all Request files

---
+++ EduCore/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module EduCore {
+    requires static lombok;
+    requires jakarta.persistence;
+    requires jakarta.validation;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.web;
+    requires org.hibernate.orm.core;
+    requires io.swagger.v3.oas.annotations;
+    requires spring.boot.autoconfigure;
+    requires io.github.cdimascio.dotenv.java;
+}
